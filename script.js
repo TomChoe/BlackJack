dbe8c3279ddb8f9446055ba3ad5c85c4c6df6ef2
TomChoe <doodeitstom@yahoo.com>
2018-02-07 22:24:30 -0500

    revised ace code

--- script.js
+++ script.js
@@ -26,32 +26,33 @@ let bank = Number(sessionStorage.getItem('bank')) || 100;
 let bet = 0;
 
 const imgNum = "./images/numbercards/";       // images src
-const imgFace = "./images/facecards/"
+const imgFace = "./images/facecards/";
+const next = $('<button id= "next">Next Round</button>').click(reset);
 
 function createDeck() {
 	this.value = [2, 3, 4, 5, 6, 7, 8, 9, 10];
 	this.face = ['A', 'J', 'Q', 'K'];
 	this.suit = ['C', 'D', 'H', 'S'];
 
-	for (s = 0; s < suit.length; s++) {
-		for (i = 0; i < value.length; i ++) {
+	for (let s = 0; s < suit.length; s++) {
+		for (let i = 0; i < value.length; i ++) {
 			deck.push(new card(value[i], suit[s], imgNum+value[i]+suit[s]+'.jpg'));	
 		}
 	}	
 
-	for (s = 0; s < suit.length; s++) {
-		for (f = 0; f < face.length; f++) {
+	for (let s = 0; s < suit.length; s++) {
+		for (let f = 0; f < face.length; f++) {
 			deck.push(new faceCard(face[f], suit[s], 10, imgFace+face[f]+suit[s]+'.jpg'));
 		}
 	}
 
-	for (i = 0; i < deck.length; i++) {
+	for (let i = 0; i < deck.length; i++) {
 		if (deck[i].face === 'A') {
 			deck[i].value = 11;
 		}
 	}
 
-	$('.deck').append("<img id = 'deckCard' src ='./images/cardback.jpg'/>")
+	$('.deck').append("<img id = 'deckCard' src ='./images/grayback.jpg'/>")
 	shuffleDeck()
 }
 
@@ -65,10 +66,11 @@ function shuffleDeck(array) {
 }
 
 function deal() {
-	bank = parseInt(bank);
-	bet = parseInt(bet);
+	bank = Number(bank);
+	bet = Number(bet);
 	
-	for (i = 0; i < 2; i++) {
+	// dealing for player and dealer
+	for (let i = 0; i < 2; i++) {
 		playerHand.push(deck.pop());
 		$('.playerCards').append('<img id = "playCard" src = '+playerHand[i].img+'>')
 		dealerHand.push(deck.pop());
@@ -82,14 +84,11 @@ function deal() {
 		$('.buttons').remove();
 		$('#bank').html(bank);
 		bank = bank + bet;
-		reset();
+		$('#reset').append(next);
+	} else if (currentHand > 21 && playerHand[0].face === "A") {
+		currentHand -= 10;
 	}
-	for (c = 0; c < playerHand.length; c++) {
-		if (currentHand > 21 && playerHand[c].face === 'A') {
-			playerHand[c].value = 1;
-		}
 
-	}
 	$('#btnDeal').remove();
 	const hitB = $('<button id = "hit">HIT</button>').click(hit);						// credit to Instructors
 	const standB = $('<button id = "stand">STAND</button>').click(stand)
@@ -98,52 +97,57 @@ function deal() {
 }
 
 function wager() {							        	// using the bet button to store a bet and return or lose based on 
-	bet = 0;
 	bet = $('#number').val();
 	const dealB = $('<button id = "btnDeal">DEAL</button>').click(deal);
 	
 	if (bet > bank) {
 		alert("You can't bet more than what you have")
 		window.location.reload();
-	}
+	} 
 	console.log(bet);
 	$('.wager').remove();
 	$('.buttons').append(dealB);
 }
 
 function hit() {
+	bank = Number(bank);
+	bet = Number(bet);
 	playerHand.push(deck.pop());
 	$('.playerCards').append('<img id = "playCard" src = '+playerHand[playerHand.length-1].img+'>')
+	let pHand = playerHand.reduce((a, i) => {
+		 a += i.value
+		 return a
+	}, 0);
 
-	let pHand = 0;
+	if (pHand > 21 && playerHand[playerHand.length-1].face === "A") {       // checks value of the new card
+		pHand -= playerHand[playerHand.length-1].value;
+		playerHand[playerHand.length-1].value = 1;
+		pHand += playerHand[playerHand.length-1].value;
+	} 
 
-	for (p = 0; p < playerHand.length; p++) {
-		bank = parseInt(bank);
-		bet = parseInt(bet);
-		console.log('in the loop for hand length')
-		pHand += playerHand[p].value;
-		
-		if (pHand > 21) {
+	for (let p = 0; p < playerHand.length; p++) {						// checks to see if aces are in hand 
+		if (pHand > 21 && playerHand[p].face === "A") {
+			pHand -= playerHand[p].value;
+			playerHand[p].value = 1;
+			pHand += playerHand[p].value;
+		} else if (pHand > 21) {
 			alert(`Busted! you have ${pHand} You lose ${bet}`);
 			bank = bank - bet;
 			$('.buttons').remove();
 			$('#bank').html(bank);
-			reset()
+			$('#reset').append(next);
+			return;
 		}
-	
-		if (pHand > 21 && playerHand[p].face === 'A') {
-			playerHand[p].value = 1;
-		}
-	}
 	$('#total').text(bank);
+	}
 }
 
 function stand() {
 	$('.buttons').remove();
 	$('#hidden').remove();
 	$('.dealerCards').append("<img id = 'dealt' src = "+dealerHand[0].img+">")
-	bank = parseInt(bank);
-	bet = parseInt(bet);
+	bank = Number(bank);
+	bet = Number(bet);
 	let dealer = dealerHand[0].value + dealerHand[1].value;
 	let player = playerHand.reduce((a, i) => {
 		 a += i.value
@@ -154,6 +158,18 @@ function stand() {
 			dealerHand.unshift(deck.pop());
 			dealer += dealerHand[0].value;
 			$('.dealerCards').append("<img id = 'dealt' src = "+dealerHand[0].img+">")
+			if (dealer > 21 && dealerHand[0].face === 'A') {
+				dealer -= dealerHand[0].value;
+				dealerHand[0].value = 1;
+				dealer += dealerHand[0].value;
+			} 	
+			for (let d = 0; d < dealerHand.length; d++) {
+				if (dealer > 21 && dealerHand[d].face === 'A') {
+					dealerHand -= dealerHand[d].value;
+					dealerHand[d].value = 1;
+					dealerHand += dealerHand[d].value;
+				}
+			}
 			if (dealer > 21) {
 				bank = bank + bet;
 				alert(`dealer busted! You win $${bet}`);
@@ -164,11 +180,6 @@ function stand() {
 				alert('Push!')
 			}
 	}
-		for (d = 0; d < dealerHand.length; d++) {
-			if (dealer > 21 && dealerHand[d].face === 'A') {
-				dealerHand[d].value = 1;
-			}
-		}
 			if (player > dealer) {
 				bank = bank + bet;
 				alert(`Player wins! $${bet} has been added to the bank`);
@@ -177,27 +188,26 @@ function stand() {
 				alert(`Dealer wins! You lose $${bet}`);
 			}
 	$('#total').html(bank)
-	reset();
+	$('#reset').append(next);
 }
 
 function doubleDown() {
-	$('.buttons').remove();
 	bet = bet * 2;
+	$('#hit').remove();
+	$('#double').remove();
 	hit();
-	stand();
 }
 
 function reset() {
-	if (bank === 0) {
+	if (bank < 1) {
 		alert('You are broke, you may need help') 
 		document.location.reload();
 	}
-	alert('Click ok to play again')
 	sessionStorage.setItem('bank', bank);
 	window.location.reload();
 }
 
-$( document ).ready(function() {
+$(document).ready(function() {
 	console.log('ready');
     createDeck();
     $('.bank').append('<div id = "total">Current bank total $ '+bank+'</div>')
